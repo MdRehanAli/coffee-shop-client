772f262f3e715c0d826156e9f5e6e814f107653a
MdRehanAli <rehanali21d@gmail.com>
2024-04-29 01:44:17 +0600

    Added SignIn and singnUp page with authentication

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@
 import { useLoaderData } from 'react-router-dom'
 import './App.css'
 import CoffeeCard from './components/CoffeeCard'
+import { useState } from 'react'
 
 function App() {
 
-  const coffees = useLoaderData()
+  const loadedCoffees = useLoaderData()
+  const [coffees, setCoffees] = useState(loadedCoffees)
+
   return (
     <div className='m-20'>
       <h1 className='text-6xl'>Hot Coffees: {coffees.length}</h1>
       <div className='grid md:grid-cols-2 gap-4'>
         {
-          coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+          coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee} coffees={coffee} setCoffees={setCoffees} ></CoffeeCard>)
         }
       </div>
     </div>
