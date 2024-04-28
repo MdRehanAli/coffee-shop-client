21bb1b61181300404de4262a6f0fd93eb12c1934
MdRehanAli <rehanali21d@gmail.com>
2024-04-29 04:27:18 +0600

    added firebase

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <h1 className='text-6xl'>Hot Coffees: {coffees.length}</h1>
       <div className='grid md:grid-cols-2 gap-4'>
         {
-          coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee} coffees={coffee} setCoffees={setCoffees} ></CoffeeCard>)
+          coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee} coffees={coffee} setCoffees={set} ></CoffeeCard>)
         }
       </div>
     </div>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from "../providers/AuthProvider";
 
 const SignUp = () => {
 
-    const {createUser} = useContext(AuthContext)
+    const { createUser } = useContext(AuthContext)
 
     const handleSignUp = event => {
         event.preventDefault()
@@ -13,14 +13,32 @@ const SignUp = () => {
         const password = form.password.value
         console.log(email, password)
         createUser(email, password)
-        .then(result => {
-            console.log(result.user)
+            .then(result => {
+                console.log(result.user)
 
-            // new user has been created 
-        })
-        .catch(error => {
-            console.log(error)
-        })
+                // new user has been created 
+
+                const createdAt = result.user?.metadata?.creationTime;
+
+                const user = { email, createdAt: createdAt };
+                fetch('http://localhost:5000/user', {
+                    method: "POST",
+                    headers: {
+                        "content-type": "application/json"
+                    },
+                    body: JSON.stringify(user)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.insertedId) {
+
+                            console.log("User added to the database")
+                        }
+                    })
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
     return (
